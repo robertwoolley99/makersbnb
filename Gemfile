516bbb6a1736eab53f87d592fdf2160938faee76
Topher <topherb99@gmail.com>
2019-11-18 15:35:29 +0000

    created database and linked to app.rb file

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'data_mapper'
+gem 'pg'
+gem 'dm-postgres-adapter'
+
 gem 'capybara'
 gem 'rspec'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra/base'
 
+require_relative 'model/properties.rb'
+
 class Bnb < Sinatra::Base
+  DataMapper.setup :default, "postgres://#{ENV["USER"]}@localhost/makersbnb"
+  DataMapper.finalize
+  DataMapper.auto_migrate!
+
   run! if app_file == $0
 end
