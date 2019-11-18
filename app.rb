1a0f644ccbf5423a1bd5699e62f6ff6d8a0c8dce
Emma Van Oss <e.vanoss@gmail.com>
2019-11-18 15:57:40 +0000

    Revert "Revert "add listing using form""
    This reverts commit 08d37f1647f9e3b32e5b8ae318c7551676159e1e.

--- app.rb
+++ app.rb
@@ -10,5 +10,14 @@ class Bnb < Sinatra::Base
     erb :new_space
   end
 
+  post '/listed' do
+    # add to database
+    redirect('/confirmation')
+  end
+
+  get '/confirmation' do
+    'Listing added OK'
+  end
+
   run! if app_file == $0
 end
