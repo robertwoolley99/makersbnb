127c3d3270d8cf72d7857906595ceefa49cefa2a
Topher <topherb99@gmail.com>
2019-11-19 11:13:51 +0000

    listing views cleaned up, all tests passing in correct fashion

--- spec/features/view_listings_spec.rb
+++ spec/features/view_listings_spec.rb
@@ -1,3 +1,6 @@
+require './spec/web_helpers/create_listing.rb'
+
+
 feature 'viewing listings' do
   scenario 'redirect from home page works' do
     visit '/'
@@ -5,12 +8,13 @@ feature 'viewing listings' do
     expect(page).to have_current_path('/spaces')
   end
 
-  # scenario 'the listings page has all the listing properties' do
-  #   expect(page).to have_content('Description:')
-  #   expect(page).to have_content('Price per night:')
-  #   expect(page).to have_content('Location:')
-  #   expect(page).to have_content('Available:')
-  #   expect(page).to have_content('Owner:')
-  #   expect(page).to have_content('Contact details:')
-  # end
+  scenario 'the listings page has all the listing properties' do
+    create_listing
+    expect(page).to have_content('Description:')
+    expect(page).to have_content('Price per night:')
+    expect(page).to have_content('Location:')
+    expect(page).to have_content('Available:')
+    expect(page).to have_content('Owner:')
+    expect(page).to have_content('Contact details:')
+  end
 end
