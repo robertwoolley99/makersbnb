1b5be69e5ec7cffc5ceb232d53457daa365d105e
Topher <topherb99@gmail.com>
2019-11-20 10:31:22 +0000

    attempting to connect jquery with postgres

--- public/interface.js
+++ public/interface.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
 
+  $.get('postgres://TopherBarnett@localhost/makersbnb', function(data) {
+  $('#disabled_dates').text(data.check_in);
 
-  $(".datepicker").datepicker({
-    dateFormat: 'dd-mm-yy'
+  var disabledDates = ["01-12-2019","05-12-2019","03-12-2019"]
+
+
+
+  $.datepicker.setDefaults({
+    showOn: "both",
+    beforeShowDay: function(date){
+        var string = jQuery.datepicker.formatDate('dd-mm-yy', date);
+        return [ disabledDates.indexOf(string) == -1 ]
+      }
   });
 
-  $( ".datepicker" ).datepicker("show");
+  $(".datepicker").datepicker({
+    dateFormat: 'dd-mm-yy',
+    showOn: "both"
+  });
 
+  var showOn = $( ".selector" ).datepicker( "option", "showOn" );
 });
