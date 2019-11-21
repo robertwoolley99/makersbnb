8cec6c807f86e3c45a17e2a41ee16da84e2c8f8e
Topher <topherb99@gmail.com>
2019-11-21 15:14:43 +0000

    deleted unnecessary comments

--- lib/get_dates.rb
+++ lib/get_dates.rb
@@ -3,14 +3,6 @@ def get_dates
 
   @bookings.each do |booking|
     @dates_array << booking.check_in
-
-    # date_out_modified = Date.parse(booking.check_out)
-    #
-    # date_out_modified -= 1
-    #
-    # date_out_modified_string = date_out_modified.strftime("%d-%m-%Y")
-    #
-    # @dates_array << date_out_modified_string
     check_in = Date.parse(booking.check_in)
     check_out = Date.parse(booking.check_out)
 
@@ -18,25 +10,13 @@ def get_dates
 
     i = stay - 1
     date = check_in
+
+
     i.times do
       date += 1
       @dates_array << date.strftime("%d-%m-%Y")
     end
 
-    # i = 1
-    # unless stay > 1
-    #   until i = (stay - 2)
-    #     pad_date = check_in + i
-    #     date_to_push = pad_date.strftime("%d-%m-%Y")
-    #     @dates_array << date_to_push
-    #     i += 1
-    #   end
-    # end
-
-
-
-
 
   end
-
 end
